bd217025bfe606dd56e8293ca21ed8bbb2b09e8b
profmayconsilva <profmayconsilva@gmail.com>
2026-02-03 12:53:38 -0300

    Aula 05: Desafio 016

---
+++ desafios/des016/des016.py
@@ -0,0 +1,15 @@
+from rich  import print
+
+class FUNCIONARIO:
+    def __init__(self, nome, setor, cargo):
+        self.nome = nome
+        self.setor = setor
+        self.cargo = cargo
+
+    def apresentar(self):
+        print(f':folded_hands: [blue]Salve Maria[/blue]! Olá irmãos e irmães, eu me chamo [bold] {self.nome}[/bold], sou do(a) {self.setor} e trabalho como {self.cargo}!')
+
+c1 = FUNCIONARIO('Maria', setor='Sara', cargo="Coordenadora")
+c1.apresentar()
+c2 = FUNCIONARIO('Matheus', 'Administação', cargo="Auxiliar Administrativo")
+c2.apresentar()
