ba2d9b26e2d2e52b6d808f12eec89aaf578d0390
Maycon Silva <profmayconsilva@gmail.com>
2026-02-10 11:25:19 -0300

    feat: Added empresa

--- desafios/des016/des016.py
+++ desafios/des016/des016.py
@@ -1,13 +1,14 @@
 from rich  import print
 
 class FUNCIONARIO:
+    empresa = 'Curso em Vídeo'
     def __init__(self, nome, setor, cargo):
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
 
     def apresentar(self):
-        print(f':folded_hands: [blue]Salve Maria[/blue]! Olá irmãos e irmães, eu me chamo [bold] {self.nome}[/bold], sou do(a) {self.setor} e trabalho como {self.cargo}!')
+        print(f':folded_hands: [blue]Salve Maria[/blue]! Olá irmãos e irmães, eu me chamo [bold] {self.nome}[/bold], sou do(a) {self.setor} e trabalho como {self.cargo} da empresa {__class__.empresa}!')
 
 c1 = FUNCIONARIO('Maria', setor='Sara', cargo="Coordenadora")
 c1.apresentar()
